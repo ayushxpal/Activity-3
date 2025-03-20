9d035d79163c23c9b56efd834a2d529b278e0f35
ayushxpal <ayush.pal23@lpu.in>
2025-03-20 12:14:44 +0530

    admission form

---
+++ next.js
@@ -0,0 +1,9 @@
+console.log("This is a new page");
+// function greet()
+//          {
+//             a=10;
+//             b=20;
+//             c=a+b;
+//             alert(c);
+//                 // alert("welcome to webpage");
+//          }
